31ca5890649e38df657dd2e04b654794d8e87647
JonathanKudsk <jonathankudsk@gmail.com>
2025-03-26 17:00:06 +0100

    order og delvist ordermapper er lavet

--- src/main/java/app/entities/Order.java
+++ src/main/java/app/entities/Order.java
@@ -1,28 +1,58 @@
 package app.entities;
 
 public class Order {
-    private int orderHistoryId;
     private int orderId;
     private int bottomId;
     private int toppingId;
     private int quantity;
-    private float price;
+    private int totalPrice;
 
-    public Order(int orderHistoryId, int orderId, int bottomId, int toppingId, int quantity, float price) {
-        this.orderHistoryId = orderHistoryId;
+    public Order(int orderId, int bottomId, int toppingId, int quantity, int price) {
         this.orderId = orderId;
         this.bottomId = bottomId;
         this.toppingId = toppingId;
         this.quantity = quantity;
-        this.price = price;
+        this.totalPrice = price;
     }
 
-    public Order(int bottomId, int toppingId, int quantity, float price) {
+
+    public int getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(int orderId) {
+        this.orderId = orderId;
+    }
+
+    public int getBottomId() {
+        return bottomId;
+    }
+
+    public void setBottomId(int bottomId) {
         this.bottomId = bottomId;
-        this.toppingId = toppingId;
-        this.quantity = quantity;
-        this.price = price;
     }
 
+    public int getToppingId() {
+        return toppingId;
+    }
 
+    public void setToppingId(int toppingId) {
+        this.toppingId = toppingId;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public int getTotalPrice() {
+        return totalPrice;
+    }
+
+    public void setTotalPrice(int totalPrice) {
+        this.totalPrice = totalPrice;
+    }
 }
--- src/main/java/app/persistence/OrderMapper.java
+++ src/main/java/app/persistence/OrderMapper.java
@@ -1,5 +1,6 @@
 package app.persistence;
 
+import app.entities.Order;
 import app.exceptions.DatabaseException;
 import org.postgresql.jdbc2.optional.ConnectionPool;
 
@@ -9,6 +10,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import java.time.LocalDate;
+import java.util.List;
 
 
 public class OrderMapper {
@@ -16,13 +18,11 @@ public class OrderMapper {
 
 
     public int addNewOrder(ConnectionPool connectionPool, int customerId) throws DatabaseException {
-        String sql = "INSERT INTO customer_orders(customer_id, order_date, status_id) " + "VALUES (?, ?, ?);";
+        String sql = "INSERT INTO customer_orders(customer_id, order_date, status_id) " + "VALUES (?, CURRENT_DATE, 1);";
 
         try (Connection connection = connectionPool.getConnection()) {
             try (PreparedStatement ps = connection.prepareStatement(sql)) {
                 ps.setInt(1, customerId);
-                ps.setDate(2, java.sql.Date.valueOf(LocalDate.now()));
-                ps.setInt(3, 1);
                 int rowsAffected = ps.executeUpdate();
                 if (rowsAffected == 0) {
                     System.out.println("Order could not be added");
@@ -36,7 +36,7 @@ public class OrderMapper {
         return 0;
     }
 
-    public int addNewOrderHistorys(ConnectionPool connectionPool, int customerId, int orderId, List<Order> order) throws DatabaseException {
+    public int addNewOrderHistories(ConnectionPool connectionPool, int customerId, int orderId, List<Order> order) throws DatabaseException {
 
         String sql = "INSERT INTO customer_order_history(customer_id, order_id, bottom_id, topping_id, quantity, total_price) " + "VALUES (?, ?, ?, ?, ?, ?);";
 
@@ -45,10 +45,10 @@ public class OrderMapper {
                 for (Order orderItem : order) {
                     ps.setInt(1, customerId);
                     ps.setInt(2, orderId);
-                    ps.setInt(3, Order.getBottomId());
-                    ps.setInt(4, Order.getToppingId());
-                    ps.setInt(5, Order.getQuantity());
-                    ps.setInt(6, Order.getTotalPrice());
+                    ps.setInt(3, orderItem.getBottomId());
+                    ps.setInt(4, orderItem.getToppingId());
+                    ps.setInt(5, orderItem.getQuantity());
+                    ps.setInt(6, orderItem.getTotalPrice());
                     int rowsAffected = ps.executeUpdate();
                     if (rowsAffected == 0) {
                         System.out.println("Order could not be added");
@@ -59,7 +59,7 @@ public class OrderMapper {
         } catch (SQLException ex) {
             throw new DatabaseException(ex, "Could not get orders from database");
         }
-        return;
+        return 0;
     }
 
 }
