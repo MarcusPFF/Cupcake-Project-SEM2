b1e20077f89f147fa459bea5dd44b52fccd45ab3
JonathanKudsk <jonathankudsk@gmail.com>
2025-03-31 15:15:44 +0200

    kæmpe rettelse af størelser i alle css plus tilføjelser af en headermenu css.

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -5,18 +5,12 @@ import app.config.ThymeleafConfig;
 import app.persistence.ConnectionPool;
 import io.javalin.Javalin;
 import io.javalin.rendering.template.JavalinThymeleaf;
-import app.controllers.RoutingController;
+//import app.controllers.RoutingController;
 import java.util.logging.Logger;
 
 
 public class Main {
-    private static final Logger LOGGER = Logger.getLogger(Main.class.getName());
-    private static final String USER = "postgres";
-    private static final String PASSWORD = "postgres";
-    private static final String URL = "jdbc:postgresql://localhost:5432/%s?currentSchema=public";
-    private static final String DB = "Cupcake";
 
-    private static final ConnectionPool connectionPool = ConnectionPool.getInstance(USER, PASSWORD, URL, DB);
     public static void main(String[] args) {
         // Initializing Javalin and Jetty webserver
 
@@ -24,11 +18,11 @@ public class Main {
             config.staticFiles.add("/public");
             config.jetty.modifyServletContextHandler(handler -> handler.setSessionHandler(SessionConfig.sessionConfig()));
             config.fileRenderer(new JavalinThymeleaf(ThymeleafConfig.templateEngine()));
-        }).start(7070);
+        }).start(8070);
 
         // Routing
-        app.get("/", ctx -> ctx.render("index.html"));
-        RoutingController.routes(app);
+        app.get("/", ctx -> ctx.render("signup.html"));
+        //RoutingController.routes(app);
     }
 }
 
